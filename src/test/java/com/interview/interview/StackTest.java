66acd0ab0097a20d5f50dcd657cb58274adfa07e
Mishael Harrison <mishael.harrison@gmail.com>
2021-08-31 15:12:03 -0500

    added the comments

--- src/test/java/com/interview/interview/StackTest.java
+++ src/test/java/com/interview/interview/StackTest.java
@@ -1,6 +1,9 @@
 package com.interview.interview;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class StackTest {
 
@@ -11,10 +14,93 @@ public class StackTest {
         stack = new Stack();
     }
 
-    @Test
-    public void isEmpty_ReturnsTrue_IfStackIsEmpty() {
-        assertTrue(stack.isEmpty());
-    }
+//    @Test
+//    public void isEmpty_returnsTrue_whenEmpty(){
+//
+//    }
 
+//    @Test
+//    public void isEmpty_returnsFalse_afterCallingPush(){
+//
+//    }
+
+//    @Test
+//    public void peek_returnsNull_whenEmpty(){
+//
+//    }
+
+//    @Test
+//    public void peek_returnsEntity_afterPush(){
+//
+//    }
+
+//    @Test
+//    public void pop_throwsEmptyStackException_whenEmpty(){
+//
+//    }
+
+//    @Test
+//    public void pop_returnsEntity_afterPush(){
+//
+//    }
+
+//    @Test
+//    public void peek_returnsNull_afterPop(){
+//
+//    }
+
+//    @Test
+//    public void isEmpty_returnsTrue_afterPop(){
+//
+//    }
+
+//    @Test
+//    public void pop_returnsSecondItem_afterPushCalledTwice_thanPeek_returnsFirstItem(){
+//
+//    }
+
+//    @Test
+//    public void contains_returnsTrue_whenEntityExists(){
+//
+//    }
+
+//    @Test
+//    public void contains_returnsFalse_whenEntityNotFound(){
+//
+//    }
+
+    ////////////////
+    ///  Bonus   ///
+    ////////////////
+
+//    @Test
+//    public void size_returnsLength(){
+//
+//    }
+
+//    @Test
+//    public void peekN_returnsNthValue(){
+//
+//    }
+
+//    @Test
+//    public void peekN_returnsNull_whenNoEntity(){
+//
+//    }
+
+//    @Test
+//    public void peekN_throwsIllegalArgumentException_whenGivenNegative(){
+//
+//    }
+
+//    @Test
+//    public void push_throwsIllegalArgumentException_whenGivenNull(){
+//
+//    }
+
+//    @Test
+//    public void contains_throwsIllegalArgumentException_whenGivenNull(){
+//
+//    }
 
 }
